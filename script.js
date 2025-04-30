ea1f5397c76f34c7afd306a7dacce7b7d6ccf475
MahalakshmiEvents <mahalakshmieliteevents@gmail.com>
2025-04-30 10:53:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("overlay");
+  if (menu.style.left === "0px") {
+    menu.style.left = "-250px";
+    overlay.style.display = "none";
+  } else {
+    menu.style.left = "0px";
+    overlay.style.display = "block";
+  }
 }
